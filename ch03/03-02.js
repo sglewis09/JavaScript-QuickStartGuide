5b6e0473e704d33e678c89678cad48b5a1e4c2c9
Stephen G. Lewis <sglewis@sglewis.com>
2026-02-15 23:04:54 -0500

    Added the first seven examples from chapter 3

---
+++ ch03/03-02.js
@@ -0,0 +1,12 @@
+let a = 1
+let b = "1"
+
+// Compare both values and types
+if (a === b) {
+  console.log("a and b are both equal and the same type")
+}
+
+// Compare types
+if (a == b) {
+  console.log("a and b are both 1")
+}
