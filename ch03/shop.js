ea038b21bad38972196bc11781f246d91c486805
Stephen G. Lewis <sglewis@sglewis.com>
2026-02-23 16:27:55 -0500

    Added Coffee Shop example

---
+++ ch03/shop.js
@@ -0,0 +1,14 @@
+// Display a quick message in the console
+console.log("ClydeBank Coffee Shop is now open!")
+
+// Our inventory
+let inventory = ["Regular Coffee", "Exspresso", "Cappuccino", "Latte"]
+let inventoryPrices = [3.00, 3.50, 4.00, 4.24]
+
+// Obtain reference to the menu list by ID
+let menuList = document.getElementById("coffee-menu")
+
+// Loop through the inventory and display each item in the menu list
+for (let i=0; i<inventory.length; i++) {
+  menuList.innerHTML += "<li>" + inventory[i] + " - $" + inventoryPrices[i].toFixed(2) + "</li>"
+}
