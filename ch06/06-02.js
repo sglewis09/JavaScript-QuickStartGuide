e9306e75b46da2eb5394336e4f503ed9915d0adb
Stephen G. Lewis <sglewis@sglewis.com>
2026-03-07 22:18:44 -0500

    Revised version of 06-02.js

--- ch06/06-02.js
+++ ch06/06-02.js
@@ -3,7 +3,13 @@ const robert = {
   lastName: "Oliver",
   zipCode: 12345,
   zodiacSign: "Aquarius",
+  phoneNumber: "(555) 555-1212",
+
   greeting: function() {
     console.log(`Hello, ${this.firstName}!`)
+  },
+
+  sayPhoneNumber: function() {
+    console.log(this.phoneNumber)
   }
 }
